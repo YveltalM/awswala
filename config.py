f16e24db7cd1490bd5c8b1887fdeb9a477ff92bc
YveltalM <140061357+YveltalM@users.noreply.github.com>
2023-07-24 16:39:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee1.cdx2tdrp9uvh.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "Pokemon12"
+customdb = "employee1"
+custombucket = "baltiwala1"
+customregion = "ap-south-1"
